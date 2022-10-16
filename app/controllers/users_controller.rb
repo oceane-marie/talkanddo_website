d62445c56fa2bd13e78be294b3efaeaca36ae6ee
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-10-16 19:10:06 +0900

    users#show created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @pending_reservations = Reservation.where('date >= ?', DateTime.now)
+    @upcoming_reservations = Reservation.where('date >= ?', DateTime.now).where('state = ?', 'accepted')
+  end
+
+  def past_reservations
     @past_reservations = Reservation.where('date < ?', DateTime.now)
   end
+
+  def pending_reservations
+    @pending_reservations = Reservation.where('state = ?', 'pending')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,13 @@ Rails.application.routes.draw do
   resources :teachers, only: [:index, :show, :create, :new] do
     resources :reservations, only: [:create]
   end
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    member do
+      get :past_reservations
+    end
+
+    member do
+      get :pending_reservations
+    end
+  end
 end
