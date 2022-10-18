7c51f09204b1ba43bb95e40963ef8fc44d5805d7
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-10-18 20:43:45 +0900

    js clear input not really working

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -1,12 +1,14 @@
-const btn = document.getElementById('btn-search');
-const input = document.getElementById('location');
+// const form = document.getElementById('form-location');
+// const input = document.getElementById('location');
+// const btn = document.getElementById('btn-search');
+
+// btn.addEventListener(('click'), (event) => {
+//   event.preventDefault()
 
 
-btn.addEventListener(('click'), (event) => {
-  event.preventDefault()
+//   console.log(input.value);
+//   console.log(input.outerHTML);
+//   console.log(form);
 
-
-  console.log(input.value)
-
-  input.value = "";
-})
+//   input.value = "";
+// })
